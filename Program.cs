e217462ef8f6e7e4c44b67a46f419b258b092485
JD Rowley <jdrowley1989@gmail.com>
2022-03-16 22:53:54 -0600

    created get employee code and refactor

--- Program.cs
+++ Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CatWorx.BadgeMaker
 {
   class Program
   {
+    static List<string?> GetEmployees()
+    {
+        List<string?> employees = new List<string?>();
+    //   Collect user values until the value is an empty string
+        while (true)
+        {
+            Console.WriteLine("Please enter a name: (leave empty to exit): ");
+            // Get a name from the console and assign it to a variable
+            string? input = Console.ReadLine();
+            // break if the user hits enter without typing a name
+            if (input == "")
+            {
+                break;
+            }
+            employees.Add(input);
+        }
+        return employees;
+    }
+
+    static void PrintEmployees(List<string?> employees)
+    {
+        for (int i = 0; i < employees.Count; i++) 
+        {
+            Console.WriteLine(employees[i]);
+        }
+    }
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello, World!");
+        List<string?> employees = GetEmployees();
+        PrintEmployees(employees);
     }
   }
 }
