3c458f7a0fa825b00ab6eff16de000d87dc1c249
JD Rowley <jdrowley1989@gmail.com>
2022-04-22 12:07:54 -0600

    added logic to call api and choose between calling api and adding employees

--- Program.cs
+++ Program.cs
@@ -5,37 +5,20 @@ namespace CatWorx.BadgeMaker
 {
   class Program
   {
-    static List<Employee> GetEmployees()
-    {
-        List<Employee> employees = new List<Employee>();
-        //   Collect user values until the value is an empty string
-        while (true)
-        {
-            Console.WriteLine("Enter first name (leave empty to exit): ");
-            // Get a name from the console and assign it to a variable
-            string firstName = Console.ReadLine();
-            // break if the user hits enter without typing a name
-            if (firstName == "")
-            {
-                break;
-            }
-
-            Console.Write("Enter last name: ");
-            string lastName = Console.ReadLine();
-            Console.Write("Enter ID: ");
-            int id = Int32.Parse(Console.ReadLine());
-            Console.Write("Enter Photo URL: ");
-            string photoUrl = Console.ReadLine();
-            // Create a new employee instance
-            Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
-            employees.Add(currentEmployee);
-        }
-        return employees;
-    }
-
     static void Main(string[] args)
     {
-        List<Employee> employees = GetEmployees();
+      List<Employee> employees;
+
+      Console.WriteLine("Would you like to fetch data from the API? [y/n]");
+
+      if (Console.ReadLine() == "y")
+      {
+        employees = PeopleFetcher.GetFromAPI();
+      } 
+      else 
+      {
+        employees = PeopleFetcher.GetEmployees();
+      }
         Util.PrintEmployees(employees);
         Util.MakeCSV(employees);
         Util.MakeBadges(employees);
